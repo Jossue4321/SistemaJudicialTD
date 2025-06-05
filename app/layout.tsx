8bc0d00def9fc7c4a8414450f3be8b0e5875e92f
Jossue4321 <josuelm4321@gmail.com>
2025-06-04 21:25:13 -0500

    V.1 Correccion de errores

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,18 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Sistema Judicial',
+  description: 'Creado por Jos7su',
+  openGraph: {
+  images: [
+      {
+        url: 'https://png.pngtree.com/png-vector/20241025/ourlarge/pngtree-court-equipment-law-book-and-scale-png-image_14169747.png', // Ruta a la imagen
+        width: 800,
+        height: 600,
+        alt: 'Imagen representativa del Sistema Judicial', // Descripción de la imagen
+      },
+    ],
+  }
 }
 
 export default function RootLayout({
